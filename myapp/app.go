b500fdd80ce3462c15c64be1e12c07a14142fd11
DongJunHan <gamedokdok@naver.com>
2021-02-22 00:44:31 +0900

    add restfull api

--- myapp/app.go
+++ myapp/app.go
@@ -13,11 +13,18 @@ func usersHandler(w http.ResponseWriter, r *http.Request){
 	fmt.Fprint(w,"Get UserInfo by /user/{id}")
 }
 
+func getUserInfo89Handler(w http.ResponseWriter, r *http.Request){
+	
+	fmt.Fprint(w,"User Id:89")
+}
+
+
 
 func NewHandler() http.Handler{
 	mux := http.NewServeMux()
 	mux.HandleFunc("/",indexHandler)
 
 	mux.HandleFunc("/users",usersHandler)
+	mux.HandleFunc("/users/89",getUserInfo89Handler)
 	return mux
 }
--- myapp/app_test.go
+++ myapp/app_test.go
@@ -37,3 +37,16 @@ func TestUsers(t *testing.T){
 	assert.Contains(string(data),"Get UserInfo")
 	//assert.Equal("Hello World",string(data))
 }
+func TestGetUserInfo(t *testing.T){
+	assert := assert.New(t)
+
+	ts := httptest.NewServer(NewHandler())
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL+"/users/89")
+	assert.NoError(err)
+	assert.Equal(http.StatusOK,res.StatusCode)
+
+	data, _ := ioutil.ReadAll(res.Body)
+	assert.Equal(string(data),"User Id:89")
+}
