3b9a63eac154610d96f8f383ad0baed78dddefdf
DongJunHan <gamedokdok@naver.com>
2021-03-01 23:29:00 +0900

    gorilla mux를 활용하여 url path값 파싱해오기 추가

--- myapp/app.go
+++ myapp/app.go
@@ -3,6 +3,8 @@ package myapp
 import(
 	"net/http"
 	"fmt"
+	//"strings"
+	"github.com/gorilla/mux"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request){
@@ -13,18 +15,21 @@ func usersHandler(w http.ResponseWriter, r *http.Request){
 	fmt.Fprint(w,"Get UserInfo by /user/{id}")
 }
 
-func getUserInfo89Handler(w http.ResponseWriter, r *http.Request){
-	
-	fmt.Fprint(w,"User Id:89")
+func getUserInfoHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Printf("path : %s\n",r.URL.Path)
+	//slice := strings.Split(r.URL.Path,"/")
+	vars := mux.Vars(r)
+	fmt.Fprint(w,"User Id:"+vars["id"])
 }
 
 
 
 func NewHandler() http.Handler{
-	mux := http.NewServeMux()
+	mux := mux.NewRouter()
+	//mux := http.NewServeMux()
 	mux.HandleFunc("/",indexHandler)
 
 	mux.HandleFunc("/users",usersHandler)
-	mux.HandleFunc("/users/89",getUserInfo89Handler)
+	mux.HandleFunc("/users/{id:[0-9]+}",getUserInfoHandler)
 	return mux
 }
