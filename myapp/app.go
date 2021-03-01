35572b6a993d6d62d2c2041d1f94559026ab04f1
DongJunHan <gamedokdok@naver.com>
2021-03-01 23:46:01 +0900

    add restapi POST example

--- myapp/app.go
+++ myapp/app.go
@@ -3,9 +3,18 @@ package myapp
 import(
 	"net/http"
 	"fmt"
-	//"strings"
+	"time"
+	"encoding/json"
 	"github.com/gorilla/mux"
 )
+
+type User struct{
+	ID int `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName string `json:"last_name"`
+	Email string `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
 
 func indexHandler(w http.ResponseWriter, r *http.Request){
 	fmt.Fprint(w,"Hello World")
@@ -22,6 +31,21 @@ func getUserInfoHandler(w http.ResponseWriter, r *http.Request){
 	fmt.Fprint(w,"User Id:"+vars["id"])
 }
 
+func createUserHandler(w http.ResponseWriter, r *http.Request){
+	user := new(User)
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w,err)
+		return
+	}
+	//Created User
+	user.ID = 2
+	user.CreatedAt = time.Now()
+	w.WriteHeader(http.StatusCreated)
+	data, _ := json.Marshal(user)
+	fmt.Fprint(w,string(data))
+}
 
 
 func NewHandler() http.Handler{
@@ -29,7 +53,8 @@ func NewHandler() http.Handler{
 	//mux := http.NewServeMux()
 	mux.HandleFunc("/",indexHandler)
 
-	mux.HandleFunc("/users",usersHandler)
+	mux.HandleFunc("/users",usersHandler).Methods("GET")
+	mux.HandleFunc("/users",createUserHandler).Methods("POST")	
 	mux.HandleFunc("/users/{id:[0-9]+}",getUserInfoHandler)
 	return mux
 }
