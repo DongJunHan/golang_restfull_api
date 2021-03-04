826696d553eaf197d57bbdd8148f247eb7fe19a3
DongJunHan <gamedokdok@naver.com>
2021-03-04 23:18:25 +0900

    restful api 업데이트 구현 마무리.

--- myapp/app.go
+++ myapp/app.go
@@ -88,9 +88,48 @@ func deleteUserHandler(w http.ResponseWriter, r *http.Request){
 	fmt.Fprint(w,"Deleted User ID : ",id)
 
 }
-
+//update를 하는 로직은 대표적으로 두가지정도 있음.
+//1. update시도후, 업데이트해야할 정보가 없으면 create함
+//2. update시도후, 업데이트해야할 정보가 없으면 error를 반환
 func updateUserHandler(w http.ResponseWriter, r *http.Request){
+	updateUser := new(User)
+	err := json.NewDecoder(r.Body).Decode(updateUser)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w,err)
+		return
+	}
+
+	user, ok := userMap[updateUser.ID]
+	if !ok{
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "No User ID : ",updateUser.ID)
+		return
+	}
+	//해당 유효성 검사의 단점.
+	//클라이언트가 실제 값을 지우고싶어서 빈문자열을 보냈는지
+	//기존의 데이터를 유지하고싶어서 해당 key값을 보내지않아 
+	//default값으로 빈문자열이 왔는지 알방법이 필요함.
+	// 실무에서는 update용 구조체를 따로 만들어서 관리한다고한다.
+	// 업데이트용 구조체는 각 key값에 대응되는 flag값을 보관할 수 있는
+	// key를 새로 만듦.
+	if updateUser.FirstName != ""{
+		user.FirstName = updateUser.FirstName
+	}
+	if updateUser.LastName != ""{
+		user.LastName = updateUser.LastName
+	}	
+	if updateUser.Email != ""{
+		user.Email = updateUser.Email
+	}
+	//if updateUser.FirstName != ""{
+	//	user.FirstName = updateUser.FirstName
+	//}
+	w.Header().Add("Content-Type","application/json")
 	w.WriteHeader(http.StatusOK)
+	data, _ := json.Marshal(user)
+	fmt.Fprint(w, string(data))
+
 }
 
 func NewHandler() http.Handler{
--- myapp/app_test.go
+++ myapp/app_test.go
@@ -3,6 +3,7 @@ package myapp
 import(
 	"testing"
 	"net/http/httptest"
+	"fmt"
 	"net/http"
 	"io/ioutil"
 	"strings"
@@ -124,5 +125,37 @@ func TestUpdateUser(t *testing.T){
 	res, err := http.DefaultClient.Do(req)
 	assert.NoError(err)
 	assert.Equal(http.StatusOK,res.StatusCode)
+	data, _ := ioutil.ReadAll(res.Body)
+	//해당 데이터가 없을 경우.
+	assert.Contains(string(data),"No User ID : 1")
+	//post로 create를 요청함.
+	res, err = http.Post(ts.URL+"/users","application/json",strings.NewReader(`{"first_name":"Han","last_name":"dong","email":"gamedokdok@naver.com"}`))
+	assert.NoError(err)
+	assert.Equal(http.StatusCreated, res.StatusCode)
+	
+	//get data
+	user := new(User)
+	err = json.NewDecoder(res.Body).Decode(user)
+	assert.NoError(err)
+	assert.NotEqual(0,user.ID)
+	//update data
+	//update시 업데이트하고싶은 데이터만 보내게 되면, 나머지 데이터는
+	//default값으로 바꾸게 된다. string은 빈문자열로 보내게되고
+	//기존의 정보를 빈문자열로 덮어쓰게 된다.
+	//따라서 기존의 데이터는 그대로 가지고 가야한다.
+	updateStr := fmt.Sprintf(`{"id" : %d, "first_name":"updated"}`,user.ID)
+	req, _ = http.NewRequest("PUT",ts.URL+"/users",strings.NewReader(updateStr))
+	res, err = http.DefaultClient.Do(req)
+	assert.NoError(err)
+	assert.Equal(http.StatusOK,res.StatusCode)
+	//get data
+	updateUser := new(User)
+	err = json.NewDecoder(res.Body).Decode(updateUser)
+	assert.NoError(err)
 
+	assert.Equal(updateUser.ID,user.ID)
+	assert.Equal("updated",updateUser.FirstName)
+	assert.Equal(user.LastName,updateUser.LastName)
+	assert.Equal(user.Email,updateUser.Email)
+	
 }
